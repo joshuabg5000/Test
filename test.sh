e5960a8619b418e1bdc54336ee7735c4d3a7299e
joshuabg5000 <joshuabg5000@gmail.com>
2014-10-12 10:48:46 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,5 +3,8 @@ java -version
 yes | sudo apt-get install bison build-essential bzip2 curl dpkg-dev flex g++-multilib git git-review gnupg gperf lib32bz2-1.0 lib32bz2-dev lib32ncurses5-dev lib32readline-gplv2-dev lib32z1-dev libbz2-1.0 libbz2-dev libc6-dev libghc-bzlib-dev libgl1-mesa-dev libgl1-mesa-glx:i386 libncurses5-dev libreadline6-dev libreadline6-dev:i386 libx11-dev:i386 libxml2-utils lzop mingw32 pngcrush python-markdown schedtool squashfs-tools tofrodos x11proto-core-dev xsltproc zip zlib1g-dev zlib1g-dev:i386
 sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
 mkdir ~/bin && curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo && chmod a+x ~/bin/repo
+mkdir ~/android && cd ~/android
+repo init -u git://github.com/PAC-man/pacman.git -b pac-4.4
+repo sync
 echo test
 ls
