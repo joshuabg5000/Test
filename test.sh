a625854bb3e5fda1b39a14e0ae9b1570f6ef7e26
joshuabg5000 <joshuabg5000@gmail.com>
2014-10-12 11:09:43 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,22 @@
-#/bin/bash
+#!/bin/bash
+
+echo "this is for Ubuntu 12.04"
+
+
+sudo apt-get install -y gcc ccache
+sudo apt-get install -y git-core gnupg flex bison gperf build-essential zip curl libc6-dev x11proto-core-dev libreadline6-dev:i386 libgl1-mesa-glx:i386 g++-multilib mingw32 openjdk-6-jdk tofrodos python-markdown libxml2-utils schedtool pngcrush xsltproc zlib1g-dev:i386 python-software-properties
+sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
+sudo add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main"
+sudo apt-get update
+sudo apt-get install -y oracle-java6-installer
+ls -la /etc/alternatives/java* && ls -la /etc/alternatives/jar
 java -version
-mkdir ~/bin && curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo && chmod a+x ~/bin/repo
-mkdir ~/android && cd ~/android
-repo init -u git://github.com/PAC-man/pacman.git -b pac-4.4
-repo sync
-echo test
-ls
+mkdir ~/bin
+PATH=~/bin:$PATH
+curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo && chmod a+x ~/bin/repo
+
+echo "export USE_CCACHE=1" >> .bashrc
+source ~/.bashrc
+prebuilts/misc/linux-x86/ccache/ccache -M 50G
+
+echo "finished. You can now add your favorite Repo. Have fun"
