dbc0a45b2c7cb5297342dfe42106065ee8267ba4
joshuabg5000 <joshuabg5000@gmail.com>
2014-10-12 10:27:07 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 sudo apt-get purge openjdk-\* icedtea-\* icedtea6-\*
 echo test
 yes '' | sudo add-apt-repository ppa:webupd8team/java
-
+yes | sudo apt update && sudo apt install oracle-java6-installer
