37a20782d46f6290907542de44f2b03d2bb90ba2
joshuabg5000 <joshuabg5000@gmail.com>
2014-10-12 11:13:59 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ echo "this is for Ubuntu 12.04"
 sudo apt-get install -y gcc ccache
 sudo apt-get install -y git-core gnupg flex bison gperf build-essential zip curl libc6-dev x11proto-core-dev libreadline6-dev:i386 libgl1-mesa-glx:i386 g++-multilib mingw32 openjdk-6-jdk tofrodos python-markdown libxml2-utils schedtool pngcrush xsltproc zlib1g-dev:i386 python-software-properties
 sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
-sudo apt purge openjdk-\* icedtea-\* icedtea6-\*
+sudo apt-get purge openjdk-\* icedtea-\* icedtea6-\*
 sudo add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main"
 sudo apt-get update
 sudo apt-get install -y oracle-java6-installer
