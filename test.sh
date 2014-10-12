a57e476c549819536d4a89b80b28a536a4caa64d
joshuabg5000 <joshuabg5000@gmail.com>
2014-10-12 11:45:26 -0400

    Make logs shorter

--- test.sh
+++ test.sh
@@ -3,13 +3,13 @@
 echo "this is for Ubuntu 12.04"
 
 
-sudo apt-get install -y gcc ccache
-sudo apt-get install -y git-core gnupg flex bison gperf build-essential zip curl libc6-dev x11proto-core-dev libreadline6-dev:i386 libgl1-mesa-glx:i386 g++-multilib mingw32 openjdk-6-jdk tofrodos python-markdown libxml2-utils schedtool pngcrush xsltproc zlib1g-dev:i386 python-software-properties
+sudo apt-get install -y gcc -qq ccache
+sudo apt-get install -y -qq git-core gnupg flex bison gperf build-essential zip curl libc6-dev x11proto-core-dev libreadline6-dev:i386 libgl1-mesa-glx:i386 g++-multilib mingw32 openjdk-6-jdk tofrodos python-markdown libxml2-utils schedtool pngcrush xsltproc zlib1g-dev:i386 python-software-properties
 sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
 sudo apt-get purge openjdk-\* icedtea-\* icedtea6-\*
 sudo add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main"
-sudo apt-get update
-sudo apt-get install -y oracle-java6-installer
+sudo apt-get update -qq
+sudo apt-get install -qq -y oracle-java6-installer
 ls -la /etc/alternatives/java* && ls -la /etc/alternatives/jar
 java -version
 mkdir ~/bin
@@ -21,6 +21,6 @@ source ~/.bashrc
 prebuilts/misc/linux-x86/ccache/ccache -M 50G
 
 echo "finished. You can now add your favorite Repo. Have fun"
-yes "" | repo init -u git://github.com/PAC-man/pacman.git -b pac-4.4
+yes "" | repo init -q -u git://github.com/PAC-man/pacman.git -b pac-4.4
 echo syncing
 repo sync -q
