6f3cb6f21bc159423c7122f74db5bdfb5c985196
joshuabg5000 <joshuabg5000@gmail.com>
2014-10-12 17:41:05 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+mkdir ~/bin
+PATH=~/bin:$PATH
+curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo && chmod a+x ~/bin/repo
+
+echo "export USE_CCACHE=1" >> .bashrc
+source ~/.bashrc
+prebuilts/misc/linux-x86/ccache/ccache -M 50G
 java -version
 ./build-pac.sh -f wx_na_wf
